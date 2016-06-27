22aaba50346fd99e9ed8efa2aa8d960d0fd58ccb
andy-mueller <andreas.b.mueller@freenet.de>
2016-06-27 14:42:00 +0200

    fixed script to sue olution

--- run-test.ps1
+++ run-test.ps1
@@ -1,3 +1,4 @@
 
-& "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" .\cli.dispatcher.test\cli.dispatcher.test.csproj /t:rebuild
+& "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" .\cli.dispatcher.sln /t:clean
+& "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" .\cli.dispatcher.sln /t:build
 .\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe .\cli.dispatcher.test\bin\Debug\cli.dispatcher.test.dll
