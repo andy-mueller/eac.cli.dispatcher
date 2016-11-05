7b19e69e143dcaba71c26b36872c558709ee96b9
andy-mueller <andreas.b.mueller@freenet.de>
2016-11-05 20:48:00 +0100

    Added the cutting of esections from a ta string template

--- eac.cli.dispatcher.test/StringTemplateTest.cs
+++ eac.cli.dispatcher.test/StringTemplateTest.cs
@@ -34,5 +34,87 @@ namespace cli.dispatcher.test
             Assert.Equal("lalala VALUEjfsj sölVALUE2fkkf", result);
         }
 
+        [Fact]
+        public void CutSection()
+        {
+            var template = Template.of("lalala %CUT%jfsj söl%CUT%fkkf");
+
+            var properties = new Dictionary<string, string>()
+            {
+                { "KEY", "VALUE" },
+                { "CUT","any" }
+            };
+
+            IList<string> cutProps = new List<string> { "CUT" };
+            var result = template.Cut(properties, cutProps);
+
+            Assert.Equal("lalala fkkf", result);
+        }
+        [Fact]
+        public void CutsSectionOnlyForSpecifiedProperties()
+        {
+            var template = Template.of("la%CUT2%lala%CUT2%aslfjaljf %CUT%jfsj söl%CUT%fkkf");
+
+            var properties = new Dictionary<string, string>()
+            {
+                { "KEY", "VALUE" },
+                { "CUT","any" }
+            };
+
+            IList<string> cutProps = new List<string> { "CUT" };
+            var result = template.Cut(properties, cutProps);
+
+            Assert.Equal("la%CUT2%lala%CUT2%aslfjaljf fkkf", result);
+        }
+        [Fact]
+        public void CutsMultipleCuts()
+        {
+            var template = Template.of("lalala %CUT%jfsj söl%CUT%fkk%CUT2%fsfdasf%CUT2% safs sfsf");
+
+            var properties = new Dictionary<string, string>()
+            {
+                { "KEY", "VALUE" },
+                { "CUT","any" },
+                { "CUT2","any" }
+            };
+
+            IList<string> cutProps = new List<string> { "CUT", "CUT2" };
+            var result = template.Cut(properties, cutProps);
+
+            Assert.Equal("lalala fkk safs sfsf", result);
+        }
+        [Fact]
+        public void ignoresUnknownCut()
+        {
+            var template = Template.of("lalala %CUT%jfsj söl%CUT%fkk fsf");
+
+            var properties = new Dictionary<string, string>()
+            {
+                { "KEY", "VALUE" },
+                { "CUT","any" }
+            };
+
+            IList<string> cutProps = new List<string> { "NOT_NOWN", "CUT" };
+            var result = template.Cut(properties, cutProps);
+
+            Assert.Equal("lalala fkk fsf", result);
+        }
+        [Fact]
+        public void CutsMultipleSections()
+        {
+            var template = Template.of("lalala %CUT%jfsj söl%CUT%fkk%CUT%fsfdasf%CUT% safs sfsf");
+
+            var properties = new Dictionary<string, string>()
+            {
+                { "KEY", "VALUE" },
+                { "CUT","any" }
+            };
+
+            IList<string> cutProps = new List<string> { "CUT" };
+            var result = template.Cut(properties, cutProps);
+
+            Assert.Equal("lalala fkk safs sfsf", result);
+        }
+
     }
 }
--- eac.cli.dispatcher/usecase/StringTemplate.cs
+++ eac.cli.dispatcher/usecase/StringTemplate.cs
@@ -27,5 +27,21 @@ namespace cli.dispatcher
         {
             return template.Replace(string.Format("%{0}%", property.Key), property.Value);
         }
+
+        public string Cut(Dictionary<string, string> properties, IEnumerable<string> cuts)
+        {
+            String reducedTemplate = template;
+            foreach(string cut in cuts) { 
+                string cutProp = string.Format("%{0}%", cut);
+                int first = reducedTemplate.IndexOf(cutProp);
+                while (first > 0)
+                {
+                    int last = reducedTemplate.IndexOf(cutProp, first + 1) + cutProp.Length;
+                    reducedTemplate = reducedTemplate.Remove(first, last - first);
+                    first = reducedTemplate.IndexOf(cutProp);
+                }
+            }
+            return reducedTemplate;
+        }
     }
 }
